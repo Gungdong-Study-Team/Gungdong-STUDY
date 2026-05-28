9960ed01f67aaf4a10139b480474db7a02e000b3
명현철 <151330711+MyeongHyeonCheol@users.noreply.github.com>
2026-05-28 16:51:54 +0900

    5월 28일

---
+++ hyeoncheol/algorithm/2/12914. 멀리 뛰기/Solution.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public long solution(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        long[] memo = new long[n + 1];
+        memo[1] = 1;
+        memo[2] = 2;
+        
+        
+        for (int i = 3; i <= n; i++) {
+            memo[i] = (memo[i - 1] + memo[i - 2]) % 1234567;
+        }
+        
+        return memo[n];
+    }
+}
